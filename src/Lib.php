11d53af84722466527cb8ee10ca44605126b7fad
Vasilkov Dmitriy <d.vasilckoff@yandex.ru>
2020-07-26 23:53:34 +0200

    add src/Lib.php

---
+++ src/Lib.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Brain\Games\Lib;
+
+use function cli\line;
+use function cli\prompt;
+
+const NUMBER_OF_QUESTIONS = 3;
+
+function nameQuery()
+{
+    $name = prompt("May I have your name?");
+    line("Hello, %s\n", $name);
+    return $name;
+}
+
+function taskPrint(string $task)
+{
+    line("Welcome to Brain Games!");
+    line($task);
+}
+
+function win(string $name)
+{
+    line("Congratulations, %s", $name);
+}
+
+function loss(string $name, string $answer, string $correct_answer)
+{
+    line("'$answer' is wrong answer ;(. Correct answer was '$correct_answer'.");
+    line("Let's try again, %s", $name);
+}
+
+function clearInput(string $str)
+{
+    return trim(strtolower($str));
+}
+
+function question(string $param)
+{
+    line("Question: %s", $param);
+    return clearInput(prompt("Your answer"));
+}
+
+function runGames($task, $callback)
+{
+    taskPrint($task);
+    $name = nameQuery();
+
+    $count_correct_answer = 0;
+    $result = [];
+
+    for ($i = 0; $i < NUMBER_OF_QUESTIONS; $i++) {
+        $result = $callback();
+
+        if ($result['right']) {
+            $count_correct_answer++;
+        } else {
+            break;
+        }
+    }
+
+    $count_correct_answer === NUMBER_OF_QUESTIONS
+        ? win($name)
+        : loss($name, $result['answer'], $result['correct_answer']);
+}
